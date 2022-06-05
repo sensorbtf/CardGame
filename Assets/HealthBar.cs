0f15eb36c725a15b924b3b6eba280e241f2c5462
Mateusz Łysoń <mateusz223311@gmail.com>
2022-06-05 10:33:58 +0200

    ColoredHealthBars

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -7,15 +7,21 @@ public class HealthBar : MonoBehaviour
 {
     public Slider healthSlider;
 
+    public Gradient gradient;
+    public Image fill;
+
     public void SetMaxHealth(int health)
     {
         healthSlider.maxValue = health;
         healthSlider.value = health;
+
+        fill.color = gradient.Evaluate(1.0f);
     }
 
     public void SetHealth(int health)
     {
         healthSlider.value = health;
+        fill.color = gradient.Evaluate(healthSlider.normalizedValue);
     }
 
 }
