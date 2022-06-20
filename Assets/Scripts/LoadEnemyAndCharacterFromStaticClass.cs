8d73980213f4ec889320a2a68ea7a8f2d2d3d1cf
Mateusz Łysoń <mateusz223311@gmail.com>
2022-06-20 08:33:44 +0200

    Update LoadEnemyAndCharacterFromStaticClass.cs

--- Assets/Scripts/LoadEnemyAndCharacterFromStaticClass.cs
+++ Assets/Scripts/LoadEnemyAndCharacterFromStaticClass.cs
@@ -2,18 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoadDeckAndCharacterFromStaticClass : MonoBehaviour {
-
+public class LoadEnemyDeckAndCharacterFromStaticClass : MonoBehaviour {
     void Awake()
     {
         Player p = GetComponent<Player>();
-        if (BattleStartInfo.SelectedDeck != null)
-        {
-            if (BattleStartInfo.SelectedDeck.Character!=null)
-                p.charAsset = BattleStartInfo.SelectedDeck.Character;
-            if (BattleStartInfo.SelectedDeck.Cards!=null)
-                p.deck.cards = new List<CardAsset>(BattleStartInfo.SelectedDeck.Cards);
-        }       
 
+        if (MapScreen.Instance.choiceOfEnemy == false) // when false = normal, when true = boss
+        {
+            Debug.LogWarning("LOADED NORMAL");
+            if (BattleStartInfo.EnemyDeck.Character != null)
+                p.charAsset = BattleStartInfo.EnemyDeck.Character;
+            if (BattleStartInfo.EnemyDeck.Cards != null)
+                p.deck.cards = RandomizeCardsInList(BattleStartInfo.EnemyDeck.Cards);
+        }
+        else
+        {
+            Debug.LogWarning("LOADEDBOSS");
+            if (BattleStartInfo.BossEnemyDeck.Character != null)
+                p.charAsset = BattleStartInfo.BossEnemyDeck.Character;
+            if (BattleStartInfo.BossEnemyDeck.Cards != null)
+                p.deck.cards = RandomizeCardsInList(BattleStartInfo.BossEnemyDeck.Cards);
+        }
     }
+    public List<CardAsset> RandomizeCardsInList(List<CardAsset> inputList)
+    {
+        var copy = new List<CardAsset>(inputList); // make a copy of the list:
+        copy.Shuffle(); // shuffle the list so that we are always replacing different cards:
+        if (MapScreen.Instance.choiceOfEnemy == false)
+        {
+            for (var i = 0; i < 3; i++)
+                copy[i] = GetRandomCard();
+        }
+        else
+        {
+            copy[0] = GetRandomCard(); // choosing only 1 cuz 1 card in list
+        }
+        return copy;
+    }
+    public CardAsset GetRandomCard()
+    {
+        if (MapScreen.Instance.choiceOfEnemy == false)
+            return CardCollection.Instance.allCreaturesCardsArray[Random.Range(0, CardCollection.Instance.allCreaturesCardsArray.Length)];
+        else
+            return CardCollection.Instance.allBossCreaturesCardsArray[Random.Range(0, CardCollection.Instance.allBossCreaturesCardsArray.Length)];
+    }
+
 }
